17ed4fd19c029c11a87ac71b9aeccc5f52e65b1d
Adam Abdelrahim <abdelrahimadam3@gmail.com>
2025-01-13 13:34:04 -0500

    Created RDS files (Uploaded Snapshot)

--- variables.tf
+++ variables.tf
@@ -55,3 +55,24 @@ variable "ssh_location" {
     description = "IP addressed used for ssh into server"
     type        = string
 }
+
+# RDS variables
+variable "database_snapshot_identifier" {
+    description = "database snapshot name"
+    type        = string
+}
+
+variable "database_instance_class" {
+    description = "database instance type"
+    type        = string
+}
+
+variable "database_instance_identifier" {
+    description = "database instance identifier"
+    type        = string
+}
+
+variable "multi_az_deployment" {
+    description = "create a standby db instance"
+    type        = bool
+}
